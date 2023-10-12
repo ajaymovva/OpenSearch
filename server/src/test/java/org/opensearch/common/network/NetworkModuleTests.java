2759b706cc79790f332241d8d7bf54aab19d3a26
Ajay Kumar Movva <movvaam@amazon.com>
2023-10-13 00:14:43 +0530

    Addressing Comments
    Signed-off-by: Ajay Kumar Movva <movvaam@amazon.com>

--- server/src/test/java/org/opensearch/common/network/NetworkModuleTests.java
+++ server/src/test/java/org/opensearch/common/network/NetworkModuleTests.java
@@ -426,8 +426,7 @@ public class NetworkModuleTests extends OpenSearchTestCase {
             null,
             new NullDispatcher(),
             new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS),
-            NoopTracer.INSTANCE,
-            null
+            NoopTracer.INSTANCE
         );
     }
 }
